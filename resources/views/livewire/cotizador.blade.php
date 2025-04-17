58439eda5bd101384d0c7f410cbd0accf5c68521
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-04-16 22:59:19 -0700

    Change Checkbox for Radio

--- resources/views/livewire/cotizador.blade.php
+++ resources/views/livewire/cotizador.blade.php
@@ -15,7 +15,7 @@
                     class="border-secondary bg-background has-[:checked]:bg-highlight has-[:checked]:text-background has-[:checked]:border-highlight flex cursor-pointer items-center justify-between gap-2 rounded-2xl border-2 px-2 py-4 transition-colors md:px-6">
                     <div class="flex items-center gap-2">
                         <div>
-                            <input type="checkbox"  value="{{ $modeloBase->estandar }}"
+                            <input type="radio" wire:model.live="precioBase" value="{{ $modeloBase->estandar }}"
                                 class="peer hidden">
                             <div
                                 class="border-secondary peer-checked:border-secondary group flex h-4 w-4 items-center justify-center rounded-md border-2 p-2">
@@ -25,7 +25,6 @@
 
                         <span class="select-none font-bold">Estandar</span>
                     </div>
-                    <span class="select-none"></span>
                 </label>
 
                 {{-- Plus --}}
@@ -33,7 +32,7 @@
                     class="border-secondary bg-background has-[:checked]:bg-highlight has-[:checked]:text-background has-[:checked]:border-highlight flex cursor-pointer items-center justify-between gap-2 rounded-2xl border-2 px-2 py-4 transition-colors md:px-6">
                     <div class="flex items-center gap-2">
                         <div>
-                            <input type="checkbox"  value="{{ $modeloBase->plus }}"
+                            <input type="radio" wire:model.live="precioBase" value="{{ $modeloBase->plus }}"
                                 class="peer hidden">
                             <div
                                 class="border-secondary peer-checked:border-secondary group flex h-4 w-4 items-center justify-center rounded-md border-2 p-2">
@@ -43,7 +42,6 @@
 
                         <span class="select-none font-bold">Plus</span>
                     </div>
-                    <span class="select-none"></span>
                 </label>
 
                 {{-- Delux --}}
@@ -51,7 +49,7 @@
                     class="border-secondary bg-background has-[:checked]:bg-highlight has-[:checked]:text-background has-[:checked]:border-highlight flex cursor-pointer items-center justify-between gap-2 rounded-2xl border-2 px-2 py-4 transition-colors md:px-6">
                     <div class="flex items-center gap-2">
                         <div>
-                            <input type="checkbox"  value="{{ $modeloBase->delux }}"
+                            <input type="radio" wire:model.live="precioBase" value="{{ $modeloBase->delux }}"
                                 class="peer hidden">
                             <div
                                 class="border-secondary peer-checked:border-secondary group flex h-4 w-4 items-center justify-center rounded-md border-2 p-2">
@@ -61,9 +59,6 @@
 
                         <span class="select-none font-bold">Delux</span>
                     </div>
-                    <span class="select-none">
-
-                    </span>
                 </label>
             </div>
         </div>
