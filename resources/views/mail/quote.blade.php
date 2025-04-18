1a75e2eda471af76fee5dae1c4f18c0ae73b0d85
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-04-17 22:55:54 -0700

    Fix typos in model names and improve formatting in email templates

--- resources/views/mail/quote.blade.php
+++ resources/views/mail/quote.blade.php
@@ -1,16 +1,11 @@
 <h1>Your Tiny House Quote!</h1>
 
 <p>Hello! {{$quote->name}} we will be more than happy to work with you, our sales team is gonna call you soon!</p>
-<b>Thank you for belive in us.</b>
-
+<b>Thank you for believing in us.</b>
 <h2>Resume:</h2>
 <b>Your phone: </b> {{$quote->cell_phone}}
 <b>Your Loction: </b> {{$quote->city}} {{$quote->neighborhood}}
 <b>Preferred contact time: </b> Date: {{$quote->scheduled->format('d-m-y') }} Time: {{$quote->scheduled->format('H:i') }}
 <h2>Your Quote:</h2>
 <b>Model: </b> {{$quote->model}}
-<pre>
-    {{htmlspecialchars($quote->description)}}
-<pre>
-
-
+<pre>{{htmlspecialchars($quote->description)}}<pre>
